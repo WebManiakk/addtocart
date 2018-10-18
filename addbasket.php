d96de0230225b335e3f31f07f42e2342078b81b2
zheka <36044212+WebManiakk@users.noreply.github.com>
2018-10-18 19:39:44 +0300

    штрішок

--- addbasket.php
+++ addbasket.php
@@ -5,3 +5,5 @@ CModule::IncludeModule("sale");
 if ($_REQUEST['id'] && CModule::IncludeModule("catalog")){
     Add2BasketByProductID($_REQUEST['id'], 1, array("DELAY" => "Y"), array());
 }
+
+//DELAY => Y - отложенные N - в корзину
