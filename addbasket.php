fd2e48fb60379046d35010d7e44bf37468b52376
WebManiakk <zshekaa@gmail.com>
2018-10-18 19:37:00 +0300

    first

---
+++ addbasket.php
@@ -0,0 +1,7 @@
+<?include($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+
+CModule::IncludeModule("sale");
+
+if ($_REQUEST['id'] && CModule::IncludeModule("catalog")){
+    Add2BasketByProductID($_REQUEST['id'], 1, array("DELAY" => "Y"), array());
+}
